745f316ed649ea4d82fd323e57117509f4ffe478
magipe <jymyj@naver.com>
2017-03-13 00:39:06 -0400

    Sidebar now include ? icon, that explains what they are supposed to be

--- src/js/components/SideBar.js
+++ src/js/components/SideBar.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import {Link} from 'react-router'
+import ToolTip from 'react-portal-tooltip'
 import CreateServiceModal from './CreateServiceModal'
 import CreateTeamModal from './CreateTeamModal'
 
@@ -34,8 +35,31 @@ import CreateTeamModal from './CreateTeamModal'
 /*
 need to decide which option we want
 I like the simple version
+Should this switch so the Link and Tooltips can be combined to make it simpler?
 */
 export default class SideBar extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state={
+            isPingTipActive: false
+        }
+    }
+
+    showToolTip = (event) => {
+        console.log("handling tool tip")
+        this.setState({
+            //isPingTipActive: true
+            [event.target.name]: true
+        })
+    }
+
+    hideToolTip = (event) => {
+        console.log("handling tool tip")
+        this.setState({
+            //isPingTipActive: false
+            [event.target.name]: false
+        })
+    }
     render() {
         return (
         <div className="col-xs-2 bg-inverse sidebar">
@@ -44,13 +68,47 @@ export default class SideBar extends React.Component {
                     <Link className="nav-link" activeClassName="activeLink" to={'/'}><h2 style={{'fontFamily': 'Droid Serif', "color": "gold"}}>LION PING</h2></Link>
                 </li>
                 <li className="nav-item">
-                    <Link className="nav-link" activeClassName="activeLink" to={'/Pings'}><h5>Pings</h5></Link>
+                    <div>
+                        <Link className="nav-link" activeClassName="activeLink" to={'/Pings'}><h5>Pings</h5></Link>
+                        <img id="pingQuestion" height="30" width="30" name="pingQuestion" onMouseEnter={this.showToolTip} onMouseLeave={this.hideToolTip} src={require("../../../assets/questionmark.png")} />
+                        <ToolTip position="right" parent="#pingQuestion" active={this.state.pingQuestion}>
+                            <div>
+                                <p>
+                                    Ping is a message to particular service, <br/>
+                                    indicating there was an incident that needs to be resolved
+                                </p>
+                            </div>
+                        </ToolTip>
+                    </div>
                 </li>
                 <li className="nav-item">
-                    <Link className="nav-link" activeClassName="activeLink" to={'/MyServices'}><h5>Services</h5></Link>
+                    <div>
+                        <Link className="nav-link" activeClassName="activeLink" to={'/MyServices'}><h5>Services</h5></Link>
+                        <img id="serviceQuestion" height="30" width="30" name="serviceQuestion" onMouseEnter={this.showToolTip} onMouseLeave={this.hideToolTip} src={require("../../../assets/questionmark.png")} />
+                        <ToolTip position="right" parent="#serviceQuestion" active={this.state.serviceQuestion}>
+                            <div>
+                                <p>
+                                    Service is a product that end-users are using <br/>
+                                    that needs to be managed to prevent down-time
+                                </p>
+                            </div>
+                        </ToolTip>
+                    </div>
                 </li>
                 <li className="nav-item">
-                    <Link className="nav-link" activeClassName="activeLink" to={'/MyTeams'}><h5>Teams</h5></Link>
+                    <div>
+                        <Link className="nav-link" activeClassName="activeLink" to={'/MyTeams'}><h5>Teams</h5></Link>
+                        <img id="teamQuestion" height="30" width="30" name="teamQuestion" onMouseEnter={this.showToolTip} onMouseLeave={this.hideToolTip} src={require("../../../assets/questionmark.png")} />
+                        <ToolTip position="right" parent="#teamQuestion" active={this.state.teamQuestion}>
+                            <div>
+                                <p>
+                                    Team is a group of members that may be responsible for <br/>
+                                    several different services. Team are also used to keep <br/>
+                                    an on-duty schedules
+                                </p>
+                            </div>
+                        </ToolTip>
+                    </div>
                 </li>
                 <li className="nav-item">
                     <Link className="nav-link" activeClassName="activeLink" to={'/Login'}><h5>Login/Register</h5></Link>
